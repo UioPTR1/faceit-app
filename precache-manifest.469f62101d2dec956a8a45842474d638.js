d6c03f2ec1ca35c0ce00ffdf80e41d0eed9771d1
UioPTR1 <itaymailb@gmail.com>
2020-05-17 18:31:56 +0300

    Updates

---
+++ precache-manifest.469f62101d2dec956a8a45842474d638.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "805108b7cad49ab46eb7df2627d29717",
+    "url": "/faceit-app-jsx/index.html"
+  },
+  {
+    "revision": "87c5ce83bef1bdcf679f",
+    "url": "/faceit-app-jsx/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a5b85f59e6bbe56ad4df",
+    "url": "/faceit-app-jsx/static/js/2.e958a0a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/faceit-app-jsx/static/js/2.e958a0a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87c5ce83bef1bdcf679f",
+    "url": "/faceit-app-jsx/static/js/main.5ec8eb51.chunk.js"
+  },
+  {
+    "revision": "5cb6cd3ab94dbdb82cf4",
+    "url": "/faceit-app-jsx/static/js/runtime-main.1b38ca43.js"
+  }
+]);
