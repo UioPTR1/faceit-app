8f58a1f7833f9ae3b8bce25f75c938305803c1cb
UioPTR1 <itaymailb@gmail.com>
2020-05-17 18:50:02 +0300

    added playerdata

--- src/components/PlayerProfile.jsx
+++ src/components/PlayerProfile.jsx
@@ -1,13 +1,15 @@
 import * as React from "react";
 import instance from "../axiosDefaults";
 
-const PlayerProfile = () => {
+const PlayerProfile = (id, nickname) => {
   let [playerData, setData] = React.useState(null);
 
   const fetchLiorData = async () => {
     const request = await instance.get(
-      "/players/cd8a3937-a6b5-4972-8d0b-42c790ff3132"
+      "/players/cd8a3937-a6b5-4972-8d0b-42c790ff3132/stats/csgo"
     );
+    console.log(request.data);
+    
     return request.data;
   };
 
@@ -21,7 +23,10 @@ const PlayerProfile = () => {
     "no data"
   ) : (
     <div>
-      <h1>{playerData.nickname}</h1>
+      <h1>Lior</h1>
+      <p>Average HS percentage: {playerData.lifetime["Average Headshots %"]}%</p>
+      <p>Average K/D Ratio: {playerData.lifetime["Average K/D Ratio"]}</p>
+      <p>Current Win Streak: {playerData.lifetime["Current Win Streak"]}</p>
     </div>
   );
 };
--- src/playerIDS.js
+++ src/playerIDS.js
@@ -2,7 +2,8 @@ const IDS = {
   liorID: "cd8a3937-a6b5-4972-8d0b-42c790ff3132",
   ayraID: "eb07ae0a-87c1-44c6-b348-ca18d5869ea4",
   heziID: "44f43251-739e-4c98-b2f9-dae3e0d42f9b",
-  lavanID: "5153d825-9e29-408f-a772-d13375c78bc5"
+  lavanID: "5153d825-9e29-408f-a772-d13375c78bc5",
+  itayID: "c4d82eac-dd80-4151-bac8-4580904cdcdb"
 };
 
 export default IDS;
